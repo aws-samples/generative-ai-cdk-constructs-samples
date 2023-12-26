789695dd755bd6f30a0dbce6b9200145139dc6e1
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2023-12-26 11:20:06 -0800

    fix: empty logging buckets
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- samples/document_explorer/empty_buckets.py
+++ samples/document_explorer/empty_buckets.py
@@ -12,14 +12,24 @@ if os.path.isfile(cdk_deploy_output_file):
         parsed_json = json.load(cdk_deploy_output)
 
         s3 = boto3.resource('s3')
+        client = boto3.client('s3')
 
         try:
             def empty_bucket(bucket_name):
                 bucket = s3.Bucket(bucket_name)
                 if s3.BucketVersioning(bucket_name).status == 'Enabled':
-                    bucket.object_versions.delete()
+                    bucket.object_versions.all().delete()
                 else:
                     bucket.objects.all().delete()
+
+                response_get_bucket_logging = client.get_bucket_logging(Bucket=bucket_name)
+                if 'LoggingEnabled' in response_get_bucket_logging and 'TargetBucket' in response_get_bucket_logging['LoggingEnabled']:
+                    logging_bucket = s3.Bucket(response_get_bucket_logging['LoggingEnabled']['TargetBucket'])
+                    if logging_bucket.versioning.status == 'Enabled':
+                        logging_bucket.object_versions.filter(Prefix=response_get_bucket_logging['LoggingEnabled']['TargetPrefix']).delete()
+                    else:
+                        logging_bucket.objects.filter(Prefix=response_get_bucket_logging['LoggingEnabled']['TargetPrefix']).delete()
+
                 return
 
             persistence_stack = 'PersistenceStack'
