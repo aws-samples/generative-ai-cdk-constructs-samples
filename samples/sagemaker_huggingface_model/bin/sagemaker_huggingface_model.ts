d1e7c9b9ce22f6fbac1af1ea2d557cc692908600
Dinesh Sajwan <dinsajwa@amazon.com>
2024-01-29 16:30:45 -0500

    feat(SAES tracking) added unique key in CFN template for SAES dashboard tracking

--- samples/sagemaker_huggingface_model/bin/sagemaker_huggingface_model.ts
+++ samples/sagemaker_huggingface_model/bin/sagemaker_huggingface_model.ts
@@ -7,7 +7,9 @@ const app = new cdk.App();
 new SagemakerHuggingfaceModelStack(app, 'SagemakerHuggingfaceModelStack', {
   env: {
     region: 'us-east-1'
-  }
+  },
+  description: '(uksb-1tupboc43) Sagemaker Hugging face model Stack'
+
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
