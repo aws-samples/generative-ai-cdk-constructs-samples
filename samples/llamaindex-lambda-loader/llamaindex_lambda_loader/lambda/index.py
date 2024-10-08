1d3b5a13eebf49c1e7bfba2ed7f10d5f2ac08e93
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-10-08 18:34:28 +0000

    fix: wip for testing

--- samples/llamaindex-lambda-loader/llamaindex_lambda_loader/lambda/index.py
+++ samples/llamaindex-lambda-loader/llamaindex_lambda_loader/lambda/index.py
@@ -11,8 +11,44 @@
 # and limitations under the License.
 #
 
+from llama_index.core import Document
+from llama_index.core import Settings
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex
+from llama_index.embeddings.bedrock import BedrockEmbedding
+from llama_index.llms.bedrock import Bedrock
+
 def handler(event, context):
     print("Hello from Lambda!")
     print(event)
     print(context)
+    
+    doc = Document(text='Schreck is the author of the story "Sir Popycock: Adventurousness in a Family" and went to Torrey Pines high school in San Diego California')
+    documents = []
+    documents.append(doc)
+    
+    model_name = "amazon.titan-embed-g1-text-02"
+    model = "amazon.titan-text-express-v1" 
+
+    embed_model = BedrockEmbedding(
+        model_name=model_name
+    )
+    Settings.llm = Bedrock(model=model)
+
+    storage_context = StorageContext.from_defaults()
+    
+    index = VectorStoreIndex.from_documents(
+        documents=documents,
+        show_progress=False,
+        transformations=None,
+        callback_manager=None,
+        embed_model=embed_model,
+        storage_context=storage_context,
+    )
+    
+    query_engine = index.as_query_engine()
+    
+    result1 = query_engine.query('Where did the author go to school?')
+    print(result1.response)
+    
     return (event)
--- samples/llamaindex-lambda-loader/llamaindex_lambda_loader/llamaindex_lambda_loader_stack.py
+++ samples/llamaindex-lambda-loader/llamaindex_lambda_loader/llamaindex_lambda_loader_stack.py
@@ -24,27 +24,37 @@ class LlamaindexLambdaLoaderStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # lambda_deps_layer = LlamaindexCommonDepsLayer(
-        #     self,
-        #     'LambdaLlamaIndexLayer',
-        #     runtime=aws_lambda.Runtime.PYTHON_3_12,
-        #     architecture=aws_lambda.Architecture.X86_64,
-        #     auto_upgrade=True,
-        # )
+        lambda_deps_layer = LlamaindexCommonDepsLayer(
+            self,
+            'LambdaLlamaIndexLayer',
+            runtime=aws_lambda.Runtime.PYTHON_3_11,
+            architecture=aws_lambda.Architecture.X86_64,
+            auto_upgrade=True,
+        )
         
         lambda_function = PythonFunction(
             self,
             "LambdaFunction",
-            runtime=aws_lambda.Runtime.PYTHON_3_10,
+            runtime=aws_lambda.Runtime.PYTHON_3_11,
             architecture=aws_lambda.Architecture.X86_64,
             entry=os.path.join(os.path.dirname(__file__), './lambda/'),
-            # layers=[
-            #     lambda_deps_layer,
-            #     # aws_lambda.LayerVersion.from_layer_version_arn(
-            #     #     self,
-            #     #     'PowerToolsLayer', 
-            #     #     f'arn:aws:lambda:{self.region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:60'
-            #     # )
-            # ],
+            layers=[
+                lambda_deps_layer,
+                aws_lambda.LayerVersion.from_layer_version_arn(
+                    self,
+                    'PowerToolsLayer', 
+                    f'arn:aws:lambda:{self.region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:60'
+                ),
+            ],
             timeout= Duration.minutes(2),
-        )   
+        )
+
+        lambda_function.add_to_role_policy(
+            aws_lambda.PolicyStatement(
+                actions=[
+                     "bedrock:InvokeModel",
+                     "bedrock:InvokeModelWithResponseStream",
+                ],
+                resources=["*"],
+            )
+        )
